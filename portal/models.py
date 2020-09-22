b061e83c583a6a4c27546269ce5bbbccd413e3a6
JackSnowdon <jacksnowdondrums@gmail.com>
2020-09-22 15:37:38 +0100

    created job model

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
+from accounts.models import Profile
 
 # Create your models here.
+
+class Job(models.Model):
+    role = models.CharField(max_length=255)
+    applied_on = models.CharField(max_length=255)
+    notes = models.TextField()
+    date = models.DateField(auto_now=True)
+    profile = models.ForeignKey(Profile, related_name='jobs', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.role
--- portal/views.py
+++ portal/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from .models import *
 
 # Create your views here.
 
 @login_required
 def portal(request):
     profile = request.user.profile
-    return render(request, "portal.html", {"profile": profile})
+    jobs = Job.objects.order_by("date")
+    return render(request, "portal.html", {"profile": profile, "jobs": jobs})

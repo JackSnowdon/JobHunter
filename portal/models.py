18bdffc8c0d79c143a255b56f5ca20990ba95ca9
JackSnowdon <jacksnowdondrums@gmail.com>
2020-09-25 16:10:21 +0100

    updated date fields on job model/Mobile UX updates

--- portal/models.py
+++ portal/models.py
@@ -8,7 +8,7 @@ class Job(models.Model):
     role = models.CharField(max_length=255)
     applied_on = models.CharField(max_length=255)
     notes = models.TextField()
-    date = models.DateField(auto_now=True)
+    date = models.DateField(auto_now_add=True)
     profile = models.ForeignKey(Profile, related_name='jobs', on_delete=models.CASCADE)
     APPLIED = 'Applied'
     INTERVIEW = 'Interview'
@@ -31,7 +31,12 @@ class Job(models.Model):
     )
     post_link = models.URLField(null=True, blank=True)
     company = models.CharField(max_length=255, null=True, blank=True)
+    last_updated = models.DateField(auto_now=True)
 
-    
     def __str__(self):
         return self.role
+
+
+#class Note(models.Model):
+#    job = models.ForeignKey(Job, related_name='noted', on_delete=models.CASCADE)
+
